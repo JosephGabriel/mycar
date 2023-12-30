3dc39d0bb01328887bb5b5b8c9fd5ce926e8bcbf
JosephGabriel <josegabriel.jgs@outlook.com>
2023-12-30 17:08:13 -0300

    commit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { plainToClass } from 'class-transformer';
+import { Observable, map } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializerInterceptor(dto));
+}
+
+export class SerializerInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) =>
+        plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
+    );
+  }
+}
